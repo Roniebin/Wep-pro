e8a75e5afe84486abd067dc576ba40ea0da16bf0
Ronnie <killpig00@naver.com>
2023-07-29 17:41:47 +0900

    eight commit

--- main/js/project_bulletin.js
+++ main/js/project_bulletin.js
@@ -4,14 +4,22 @@ var searching_phrase=document.getElementById("searching_phrase");
 var searching_img=document.getElementById("searching_img");
 
 var projects_title=document.querySelectorAll(".proj");
+var projects_date=document.querySelectorAll(".date");
+
+
 searching_img.onclick=function(){
     var temp=searching_phrase.value;
     console.log(temp);
     console.log(projects_title[1].childNodes[1].innerHTML);
 
     projects_title.forEach(function(item,index){
-        if(projects_title[index].childNodes[1].innerHTML.equals(temp)){
-            
+        var title=projects_title[index].childNodes[1].innerHTML;
+        if(title==temp){
+            projects_title[index].style.display = 'block';
+            projects_date[index].style.display = 'block';
+        }else {
+            projects_title[index].style.display = 'none';
+            projects_date[index].style.display = 'none';
         }
     })
 }
