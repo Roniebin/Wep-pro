ae628d02d48d418648694371e4d776a2e26ab68c
Ronnie <killpig00@naver.com>
2023-08-01 14:17:34 +0900

    ten commit

--- main/js/project_bulletin.js
+++ main/js/project_bulletin.js
@@ -55,29 +55,7 @@ function sliding(){
 
 //searching 검색이벤트 구현
 
-var searching_phrase=document.getElementById("searching_phrase");
-var searching_img=document.getElementById("searching_img");
 
-var projects_title=document.querySelectorAll(".proj");
-var projects_date=document.querySelectorAll(".date");
-
-
-searching_img.onclick=function(){
-    var temp=searching_phrase.value;
-    console.log(temp);
-    console.log(projects_title[1].childNodes[1].innerHTML);
-
-    projects_title.forEach(function(item,index){
-        var title=projects_title[index].childNodes[1].innerHTML;
-        if(title==temp){
-            projects_title[index].style.display = 'block';
-            projects_date[index].style.display = 'block';
-        }else {
-            projects_title[index].style.display = 'none';
-            projects_date[index].style.display = 'none';
-        }
-    })
-}
 
 var donga=document.querySelector('#donga');
 donga.addEventListener('mouseover',function(){
