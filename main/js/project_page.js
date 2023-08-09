577cb232f1b867b8b5c575c7aa9d7428cc2d7503
Ronnie <killpig00@naver.com>
2023-08-09 23:56:04 +0900

    aa

--- main/js/project_page.js
+++ main/js/project_page.js
@@ -1,9 +1,21 @@
 var page_btn=document.querySelectorAll(".number-button");
 
 console.log(page_btn);
+var present_page=0;
+
 page_btn.forEach(function (item,index){
     page_btn[index].addEventListener("click",function(){
         page_btn[index].style.backgroundColor="cyan";
         page_btn[index].style.color="black";
+
+        present_page=index;
+
+        page_btn.forEach(function (item,index){
+            if(index!=present_page){
+                page_btn[index].style.backgroundColor="#13161c";
+                page_btn[index].style.color="white";
+            }
+           
+        });
     });
 })
