7cb8cf96531bdd11e44acf55567128cded4c1a46
Roniebin <killpig00@naver.com>
2023-08-19 15:46:34 +0900

    main

--- main/js/sign_up.js
+++
@@ -1,9 +0,0 @@
-
-var donga=document.querySelector('.donga');
-donga.addEventListener('mouseover',function(){
-    donga.style.cursor= "pointer";
-})
-
-donga.addEventListener('click',function(){
-    location.href='main_post.html';
-})
