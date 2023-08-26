7558289a08e31a9cad127999ef7ff69d56b33771
Roniebin <killpig00@naver.com>
2023-08-26 19:04:46 +0900

    infoing

--- main/js/my_page.js
+++ main/js/my_page.js
@@ -18,6 +18,7 @@ var info_box=document.querySelectorAll(".info_box");
 info_box.forEach(function(item,index){
     info_box[index].addEventListener('mouseover',function(){
         info_box[index].style.backgroundColor="#F2F2F2";
+        console.log( info_box[index]);
     })
 
      info_box[index].addEventListener('mouseout',function(){
