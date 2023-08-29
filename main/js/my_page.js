50685602eb7d640f9ecf5fed40367ce971009394
Roniebin <killpig00@naver.com>
2023-08-29 20:45:34 +0900

    info_changed

--- main/js/my_page.js
+++ main/js/my_page.js
@@ -1,48 +1,17 @@
 
 
-var info_btn=document.querySelectorAll(".info_btn");
-info_btn.forEach(function(item,index){
-    info_btn[index].addEventListener('mouseout',function(){
-        info_btn[index].style.backgroundColor="white";
-    })
-    info_btn[index].addEventListener('mouseover',function(){
-        info_btn[index].style.backgroundColor="#F2F2F2";
-    })
-   
-})
+var my_page_options=document.querySelectorAll('.my_page_options');
+var present_option=my_page_options[0];
+present_option.style.backgroundColor="#13161c";
 
-var changing_proflie_pic=document.querySelector(".changing_profile_pic");
-
-changing_proflie_pic.addEventListener('mouseover',function(){
-    changing_proflie_pic.style.cursor="pointer";
-}
-)
-
-var info_box=document.querySelectorAll(".info_box");
-
-info_box.forEach(function(item,index){
-    info_box[index].addEventListener('mouseover',function(){
-        info_box[index].style.backgroundColor="#F2F2F2";
-        console.log( info_box[index]);
+my_page_options.forEach(function(item,index){
+    my_page_options[index].addEventListener('click',function(){
+       present_option.style.backgroundColor="white";
+       present_option.style.color="black";
+       present_option=my_page_options[index];
+       present_option.style.backgroundColor="#13161c";
+       present_option.style.color="white";
+     
     })
 
-     info_box[index].addEventListener('mouseout',function(){
-        info_box[index].style.backgroundColor="white";
-    })
-})
-
-
-var my_page_project_i_made=document.querySelectorAll('.my_page_project_i_made');
-
-my_page_project_i_made.forEach(function(item,index){
-    my_page_project_i_made[index].addEventListener('mouseover',function(){
-        my_page_project_i_made[index].style.backgroundColor="#F2F2F2";
-
-    }),
-
-    my_page_project_i_made[index].addEventListener('mouseout',function(){
-        my_page_project_i_made[index].style.backgroundColor="white";
-
-    }
-    
-)})
+   })
