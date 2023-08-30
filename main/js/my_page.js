f113ddb16a1749e1d972cfa11be72831e0a2cbac
Roniebin <killpig00@naver.com>
2023-08-30 15:02:55 +0900

    new

--- main/js/my_page.js
+++ main/js/my_page.js
@@ -1,17 +1,16 @@
 
 
 var my_page_options=document.querySelectorAll('.my_page_options');
-var present_option=my_page_options[0];
-present_option.style.backgroundColor="#13161c";
 
 my_page_options.forEach(function(item,index){
     my_page_options[index].addEventListener('click',function(){
-       present_option.style.backgroundColor="white";
-       present_option.style.color="black";
-       present_option=my_page_options[index];
-       present_option.style.backgroundColor="#13161c";
-       present_option.style.color="white";
-     
+    
+       if(index==0){
+         location.href='new_my_page.html';
+      
+       }else if(index==1){
+         location.href='new_my_page_project.html';
+        
+       }
     })
-
    })
