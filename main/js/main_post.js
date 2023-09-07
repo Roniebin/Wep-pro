639b43dd6d4f556a79d6516b08c69dc5bd7aa643
Roniebin <killpig00@naver.com>
2023-09-07 15:30:39 +0900

    main

--- main/js/main_post.js
+++ main/js/main_post.js
@@ -9,11 +9,11 @@ var no_hard2=document.querySelector("#no_hard2");
 
 no_hard1.addEventListener("click",function(){
    
-    window.scrollTo({ left: 0, top: 500, behavior: "smooth" });
+    window.scrollTo({ left: 0, top: 750, behavior: "smooth" });
 })
 
 no_hard2.addEventListener("click",function(){
-    window.scrollTo({ left: 0, top: 3250, behavior: "smooth" });
+    window.scrollTo({ left: 0, top: 3700, behavior: "smooth" });
 })
 
 
@@ -25,17 +25,58 @@ var gap=0;
 
 step_circles[0].addEventListener("click",function(){
     console.log("dd");
-    window.scrollTo({ left: 0, top: 1100, behavior: "smooth" });
+    window.scrollTo({ left: 0, top: 1510, behavior: "smooth" });
 })
 step_circles[1].addEventListener("click",function(){
-    window.scrollTo({ left: 0, top: 1850, behavior: "smooth" });
+    window.scrollTo({ left: 0, top: 2270, behavior: "smooth" });
 })
 step_circles[2].addEventListener("click",function(){
-    window.scrollTo({ left: 0, top: 2600, behavior: "smooth" });
+    window.scrollTo({ left: 0, top: 3025, behavior: "smooth" });
 })
 
 /*---------------------------------------------------------------------*/  
 
+/*위아래 슬라이딩*/
+var up_direction=document.querySelectorAll(".up_direction");
+up_direction.forEach(function(item,index){
+    up_direction[index].addEventListener("click",function(){
+      
+        if(index==0){
+            window.scrollTo({ left: 0, top: 0, behavior: "smooth" });
+        }
+        if(index==1){
+            window.scrollTo({ left: 0, top: 750, behavior: "smooth" });
+        }
+        if(index==2){
+            window.scrollTo({ left: 0, top: 1510, behavior: "smooth" });
+           
+        }
+        if(index==3){
+            window.scrollTo({ left: 0, top: 2270, behavior: "smooth" });
+        }
+      
+    })
+})
+
+var down_direction=document.querySelectorAll(".down_direction");
+down_direction.forEach(function(item,index){
+    down_direction[index].addEventListener("click",function(){
+      
+        if(index==0){
+            window.scrollTo({ left: 0, top: 1510, behavior: "smooth" });
+        }
+        if(index==1){
+            window.scrollTo({ left: 0, top: 2270, behavior: "smooth" });
+        }
+        if(index==2){
+            window.scrollTo({ left: 0, top: 3025, behavior: "smooth" });
+        }
+        if(index==3){
+            window.scrollTo({ left: 0, top: 4000, behavior: "smooth" });
+        }
+      
+    })
+})
 /* step 부분 화살표 이벤트*/
 var left=document.querySelector("#left");
 var right=document.querySelector("#right");
