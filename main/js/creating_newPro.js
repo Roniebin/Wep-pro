c87f2b893942330582064c2f92c7104de3282d67
Roniebin <killpig00@naver.com>
2023-09-29 13:25:56 +0900

    프로젝트 생성 끝

--- main/js/creating_newPro.js
+++ main/js/creating_newPro.js
@@ -5,6 +5,7 @@ console.log(profiles);
 //업로드 한 사진 개수
 let profiles_number=profiles.length;
 
+
 //사진 추가하기 버튼 
 let add_pic=document.querySelector('.add_pic');
 console.log("dd"+profiles_number);
@@ -22,29 +23,37 @@ upload_pictures.forEach(function(item,index) {
 })
 
 function change_p(index) {
-    
     profiles[index].value= upload_pictures[index].value
-    console.log(profiles[index])
 }
 
 add_pic.addEventListener('click',function(e){
-    console.log( document.querySelectorAll('.upload_profile'))
-    console.log(profiles)
-    const testDiv = document.querySelectorAll('.upload_profile')[profiles_number-2];
     
-    testDiv.childNodes[5].setAttribute('for',testDiv.childNodes[5].getAttribute('for')+"1")
-    testDiv.childNodes[7].setAttribute('id',testDiv.childNodes[7].getAttribute('id')+"1")
-    
-    testDiv.addEventListener("change",function(){
-        change_p(profiles_number-1)
-    });
-
+    const testDiv = document.querySelectorAll('.upload_profile')[profiles_number-2]
+        
     const newNode = testDiv.cloneNode(true);
-
+   
     testDiv.after(newNode);
+    let upload_div=document.querySelectorAll('.upload_profile');
     profiles=document.querySelectorAll('.upload-name');
+
     profiles_number+=1;
-    console.log(container3);
+    upload_pictures=document.querySelectorAll('.upload-hidden')
+    let upload=document.querySelectorAll('.upload')
+    upload_pictures.forEach(function(item,index) {
+       
+        upload[profiles_number-2].setAttribute('for',"ex_filename"+index)
+        upload_pictures[index].setAttribute('id',"ex_filename"+index)
+        profiles[profiles_number-1].value="파일선택"
+        
+      
+        upload_pictures[index].addEventListener('change',function(){
+            change_p(index)
+        })
+   
+    })
+    
+
+    console.log(profiles_number);
     if (profiles_number>3){
         var currentHeight = parseInt(getComputedStyle(container3).height, 10);
         var newHeight = currentHeight + 60;
